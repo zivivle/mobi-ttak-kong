40433996bc2c56cc2b5edf48884eb703a99e7203
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-03 22:03:59 +0900

    feat: add QuestionContainer component

---
+++ src/app/study/_components/QuestionContainer/QuestionContainer.tsx
@@ -0,0 +1,13 @@
+import type { PropsWithChildren } from 'react'
+import type { QuestionContainerProps } from './QuestionContainer.types'
+
+export const QuestionContainer = ({ title, children }: PropsWithChildren<QuestionContainerProps>) => {
+  return (
+    <section className="w-full flex flex-col items-center">
+      <h1 className="text-[18px] font-bold tracking-wide">{title}</h1>
+      <div className="flex flex-col gap-[40px] bg-white mt-7 rounded-[40px] min-w-[1063px] max-w-[1063px] min-h-[500px] p-[54px]">
+        {children}
+      </div>
+    </section>
+  )
+}
