f9e4ebc55b9ed9ffc7be96c517d265a27d666aa5
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-04 19:52:12 +0900

    fix: QuestionContainer 컴포넌트 minHeight 스타일 속성 제거

--- src/app/study/_components/QuestionContainer/QuestionContainer.tsx
+++ src/app/study/_components/QuestionContainer/QuestionContainer.tsx
@@ -5,7 +5,7 @@ export const QuestionContainer = ({ title, children }: PropsWithChildren<Questio
   return (
     <section className="w-full flex flex-col items-center">
       <h1 className="text-[18px] font-bold tracking-wide">{title}</h1>
-      <div className="flex flex-col gap-[40px] bg-white mt-7 rounded-[40px] min-w-[1063px] max-w-[1063px] min-h-[500px] p-[54px]">
+      <div className="flex flex-col gap-[40px] bg-white mt-7 rounded-[40px] min-w-[1063px] max-w-[1063px] p-[54px]">
         {children}
       </div>
     </section>
