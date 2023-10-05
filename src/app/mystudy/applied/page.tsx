9fc95533ccc141ddb4237a9dfcfa65000d735216
zivivle <zivivle@gmail.com>
2023-10-05 20:03:01 +0900

    feat: StudyAppliedPage 탭 퍼블리싱 추가

--- src/app/mystudy/applied/page.tsx
+++ src/app/mystudy/applied/page.tsx
@@ -1,13 +1,23 @@
+'use client'
+import { useState } from 'react'
 import { StudyAppliedCard } from './_components'
 
 export default function StudyAppliedPage() {
+  const [activeTab, setActiveTab] = useState('tab1')
+
   return (
     <div className="h-screen flex justify-center items-center  bg-primary-50">
       <div
         className="
          h-auto flex flex-col px-[100px]"
       >
-        <div className=" w-[950px] bg-white mt-[150px] flex flex-row flex-wrap justify-center items-center rounded-[40px] pb-[30px] mb-[40px] gap-x-[40px]">
+        <div className="flex flex-row justify-start items-center ml-10 font-semibold">
+          <div className="bg-primary-100 px-7 py-3 rounded-t-[20px] border border-b-0 border-primary-gray939 border-opacity-40 text-primary-gray939 transition-all duration-300 hover:bg-primary-300 hover:text-white cursor-pointer">
+            내가 만든 스터디
+          </div>
+          <div className="bg-white px-7 py-3 rounded-t-[20px]">내가 지원한 스터디</div>
+        </div>
+        <div className=" w-[950px] bg-white flex flex-row flex-wrap justify-center items-center rounded-[40px] pb-[30px] mb-[40px] gap-x-[40px]">
           <StudyAppliedCard />
           <StudyAppliedCard />
           {/* <StudyAppliedCard />
