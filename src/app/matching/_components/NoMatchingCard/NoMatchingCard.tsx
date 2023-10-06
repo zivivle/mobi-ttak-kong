98b9958787b9f58c9eab4714c9f971144092a348
zivivle <zivivle@gmail.com>
2023-10-06 13:46:03 +0900

    fix: NoMatchingCard matchingLoadingConstants 연동 추가

--- src/app/matching/_components/NoMatchingCard/NoMatchingCard.tsx
+++ src/app/matching/_components/NoMatchingCard/NoMatchingCard.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import beanIcon from '/public/image/icon-beans.png'
+import { matchingLoadingConstants } from '@/constants'
 
 export const NoMatchingCard = () => {
   return (
     <div className="w-[500px] bg-primary-50 rounded-[20px] p-[43px] mt-[30px] ">
       <div className="flex flex-col justify-center items-center">
         <Image src={beanIcon} alt="beanIcon" width={120} height={120} />
-        <div className="mt-[20px] ml-[20px] text-[24px] font-bold">매칭을 기다리고 있어요</div>
+        <div className="mt-[20px] ml-[20px] text-[24px] font-bold">{matchingLoadingConstants.nodataDescription}</div>
       </div>
     </div>
   )
