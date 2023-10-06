495f1430929c5e61b7f257bbbabe0f14a86ca0fd
zivivle <zivivle@gmail.com>
2023-10-06 12:06:18 +0900

    feat: 넥스트 어스 기본 세팅

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+const googleClientId = process.env.GOOGLE_CLIENT_ID || '' // 빈 문자열("")을 기본값으로 설정
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET || ''
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: googleClientId,
+      clientSecret: googleClientSecret,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
