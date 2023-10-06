d26562de7446b43d6174eefc3195c42c1ba45acb
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-06 19:50:57 +0900

    refactor: LevelCard에 이미지 적용

--- src/app/study/create/_step/_components/LevelCard/LevelCard.tsx
+++ src/app/study/create/_step/_components/LevelCard/LevelCard.tsx
@@ -6,7 +6,7 @@ import type { LevelCardProps } from './LevelCard.types'
 export const LevelCard = ({ level }: LevelCardProps) => {
   return (
     <Toggle className="flex flex-col gap-[20px] p-[36px] items-center justify-center w-[224px] h-[320px] rounded-[20px] bg-primary-50 data-[state=on]:bg-primary-100 data-[state=on]:text-black hover:bg-primary-100 hover:text-black">
-      <Image src={LEVEL_DATA[level].imageSrc} alt={`${level} 설명 카드`} width={570} height={570} />
+      <Image src={LEVEL_DATA[level].imageSrc} alt={`${level} 설명 카드`} width={120} height={120} />
       <div className="text-[20px] font-semibold">{level}</div>
       <div
         dangerouslySetInnerHTML={{ __html: LEVEL_DATA[level].description }}
