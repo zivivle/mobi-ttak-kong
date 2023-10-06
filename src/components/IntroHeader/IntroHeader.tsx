1b121afb34c72828064cf5294a05cc0a13293e00
zivivle <zivivle@gmail.com>
2023-10-06 22:01:24 +0900

    fix: 헤더 오류 해결 완료

--- src/components/IntroHeader/IntroHeader.tsx
+++ src/components/IntroHeader/IntroHeader.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import mainLogo from '/public/image/logo-black.png'
 import beanIcon from '/public/image/icon-beans.png'
 import Image from 'next/image'
 
 export const IntroHeader = () => {
   return (
-    <div className="fixed top-0 w-full h-[auto] flex justify-between pl-10 pr-14 py-2 bg-transparent z-1000">
+    <div className="fixed top-0 w-full h-[auto] flex justify-between pl-10 pr-14 py-2 bg-transparent z-50">
       <div className="flex flex-row gap-1 py-[5px]">
         <Image src={beanIcon} alt="딱콩 캐릭터" width={30} height={30} />
         <Image src={mainLogo} alt="딱콩로고" width={40} height={40} />
