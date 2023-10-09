6f122dcdc319161959d9e8464cf64f7016c70c71
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-09 22:41:16 +0900

    fix: LevelCard 컴포넌트 type 수정

--- src/app/study/_step/_components/LevelCard/LevelCard.tsx
+++ src/app/study/_step/_components/LevelCard/LevelCard.tsx
@@ -3,11 +3,11 @@ import { LEVEL_DATA } from '@/constants/level.constants'
 import Image from 'next/image'
 import type { LevelCardProps } from './LevelCard.types'
 
-export const LevelCard = ({ level }: LevelCardProps) => {
+export const LevelCard = ({ level, className }: LevelCardProps) => {
   return (
     <ToggleItem
       value={level}
-      className="flex flex-col gap-[20px] p-[36px] items-center justify-center w-[224px] h-[320px] rounded-[20px] bg-primary-50 focus:bg-primary-100 focus:text-black hover:bg-primary-100 hover:text-black"
+      className={`flex flex-col gap-[20px] p-[36px] items-center justify-center w-[224px] h-[320px] rounded-[20px] bg-primary-50 focus:bg-primary-100 focus:text-black hover:bg-primary-100 hover:text-black ${className}`}
     >
       <Image src={LEVEL_DATA[level].imageSrc} alt={`${level} 설명 카드`} width={120} height={120} />
       <div className="text-[20px] font-semibold">{level}</div>
