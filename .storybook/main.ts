3e11bdc407e1829696465d159488111ae709c632
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-13 21:22:33 +0900

    chore: Storybook에 절대경로 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs'
+const path = require('path')
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,6 +15,15 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || { alias: {} }
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // 절대 경로 추가
+      '@': path.resolve(__dirname, '../src/'),
+    }
+    return config
   },
 }
 export default config
