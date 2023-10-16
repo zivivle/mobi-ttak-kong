52875e4a57d88be3d83db8fe3c3db48221d789c8
zivivle <zivivle@gmail.com>
2023-10-16 22:15:54 +0900

    fix: queryClient 기본 설정 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,7 +6,18 @@ import { SessionProvider } from 'next-auth/react'
 import { PropsWithChildren } from 'react'
 
 const Providers = ({ children }: PropsWithChildren) => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: 0,
+        useErrorBoundary: true,
+      },
+      mutations: {
+        useErrorBoundary: true,
+      },
+    },
+  })
 
   return (
     <QueryClientProvider client={queryClient}>
