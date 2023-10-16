6b972b6a4b0887e4f30cf2c7eb98e7b39594d795
zivivle <zivivle@gmail.com>
2023-10-16 15:15:08 +0900

    fix: 불필요한 코드 삭제

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,11 +5,6 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { SessionProvider } from 'next-auth/react'
 import { PropsWithChildren } from 'react'
 
-//쿼리 클라이언트 세팅하기!
-type Props = {
-  children: React.ReactNode
-}
-
 const Providers = ({ children }: PropsWithChildren) => {
   const queryClient = new QueryClient()
 
