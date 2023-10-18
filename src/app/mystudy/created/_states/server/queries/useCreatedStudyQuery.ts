f7e39de512986eeafa5d0b611e6f89d53db7a5b1
zivivle <zivivle@gmail.com>
2023-10-18 16:07:17 +0900

    fix: 리액트 쿼리 옵션값을 선택적으로 넘길수 있도록 모두 변경

--- src/app/mystudy/created/_states/server/queries/useCreatedStudyQuery.ts
+++ src/app/mystudy/created/_states/server/queries/useCreatedStudyQuery.ts
@@ -1,10 +1,12 @@
 import { QUERY_KEYS } from '@/constants'
 import { StudyDataType } from '@/types'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import { UseQueryOptions, useQuery } from '@tanstack/react-query'
+import axios, { AxiosError } from 'axios'
 
 const fetcher = () => axios.get<StudyDataType[] | undefined>(QUERY_KEYS.getCreatedStudies).then(({ data }) => data)
 
-export const useCreatedStudyQuery = () => {
-  return useQuery([QUERY_KEYS.getCreatedStudies], fetcher)
+export const useCreatedStudyQuery = (options?: UseQueryOptions<StudyDataType[] | undefined, AxiosError>) => {
+  return useQuery<StudyDataType[] | undefined, AxiosError>([QUERY_KEYS.getCreatedStudies], fetcher, {
+    ...options,
+  })
 }
